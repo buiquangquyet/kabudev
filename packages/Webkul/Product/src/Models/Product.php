d37f63fc451d4b2db6485f7bc314ba3fefa53ee7
Jamie Duong <>
2021-07-23 12:17:00 +0700

    allow to get linked products by number of $limit

--- packages/Webkul/Product/src/Models/Product.php
+++ packages/Webkul/Product/src/Models/Product.php
@@ -164,26 +164,29 @@ class Product extends Model implements ProductContract
 
     /**
      * The related products that belong to the product.
+     * @var int $limit set number of products to get, -1 to get all
      */
-    public function related_products()
+    public function related_products(int $limit = 4)
     {
-        return $this->belongsToMany(static::class, 'product_relations', 'parent_id', 'child_id')->limit(4);
+        return $this->belongsToMany(static::class, 'product_relations', 'parent_id', 'child_id')->limit($limit);
     }
 
     /**
      * The up sells that belong to the product.
+     * @var int $limit set number of products to get, -1 to get all
      */
-    public function up_sells()
+    public function up_sells(int $limit = 4)
     {
-        return $this->belongsToMany(static::class, 'product_up_sells', 'parent_id', 'child_id')->limit(4);
+        return $this->belongsToMany(static::class, 'product_up_sells', 'parent_id', 'child_id')->limit($limit);
     }
 
     /**
      * The cross sells that belong to the product.
+     * @var int $limit set number of products to get, -1 to get all
      */
-    public function cross_sells()
+    public function cross_sells(int $limit = 4)
     {
-        return $this->belongsToMany(static::class, 'product_cross_sells', 'parent_id', 'child_id')->limit(4);
+        return $this->belongsToMany(static::class, 'product_cross_sells', 'parent_id', 'child_id')->limit($limit);
     }
 
     /**
